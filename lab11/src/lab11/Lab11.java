c0d7fb894b68046d25e6cfe58565adce2bdcde33
ms0194 <14bscsmsami@seecs.edu.pk>
2016-12-29 12:35:15 +0500

    Add files via upload

---
+++ lab11/src/lab11/Lab11.java
@@ -0,0 +1,18 @@
+package lab11;
+
+import java.io.*;
+import java.net.*;
+/**
+ *
+ * @author Murad
+ */
+public class Lab11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
